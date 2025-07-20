fe36c31cc1ac23b9379f35dc35f60ded4499b25b
hrakahraka <hamdihraka@gmail.com>
2025-07-20 22:02:54 +0100

    added knockback system to both enemy and player and enemy AI

--- Froglot.gd
+++ Froglot.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 var change_direction = false
 @export var speed = 200
+@export var damage_value = 20
+@export var HP_points = 150
 @export var gravity = 1500
 var direction = 1
 var player = null
 var player_pos = Vector2.ZERO
+var player_detected = false
 
 
 func _ready() :
@@ -13,14 +16,24 @@ func _ready() :
 
 
 func _physics_process(delta):
+	if $PivotNode/LineOfSight.is_colliding():
+		player_detected = true
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	else:
 		velocity.x = speed * direction
 		velocity.y -= 300
-	if $PivotNode/WallCheck.is_colliding() or not ($PivotNode/EdgeCheck.is_colliding()):
-		direction *= -1
-		$PivotNode.scale.x *= -1
+	if not player_detected :
+		if $PivotNode/WallCheck.is_colliding() or not ($PivotNode/EdgeCheck.is_colliding()):
+			direction *= -1
+			$PivotNode.scale.x *= -1
+	elif player_detected and is_on_floor():
+		if player.global_position > global_position:
+			direction = 1
+			$PivotNode.scale.x = 1
+		elif player.global_position < global_position:
+			direction = -1
+			$PivotNode.scale.x = -1
 	move_and_slide()
 	
 
--- player.gd
+++ player.gd
@@ -4,11 +4,20 @@ extends CharacterBody2D
 
 @export var speed = 400
 @export var jump_force = 600
+@export var damage_value = 50
+@export var HP_points = 200
 @export var gravity = 1500
+@onready var anim_tree =  get_node("PivotNode/AnimationTree")
+
 var double_jump = false
 var jump_window_timer = 0
 var is_attacking = false
-const JUMP_WINDOW =0.04
+var enemy = null
+var enemy_pos = Vector2.ZERO
+var hit = false
+var knockback_timer = 0.0
+const JUMP_WINDOW = 0.01
+const KNOCKBACK_DURATION = 1
 
 
 func _ready():
@@ -16,45 +25,67 @@ func _ready():
 
 
 func _physics_process(delta):
-	velocity.x = 0
+	var direction = 0
 	if Input.is_action_pressed("move_right"):
-		velocity.x += speed
+		direction += 1
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= speed
+		direction -=  1
+	if direction != 0:
+		velocity.x = direction * speed
+	elif (not hit) and is_on_floor():
+		velocity.x = 0
 	if not is_on_floor():
 		jump_window_timer += delta
-		if jump_window_timer>JUMP_WINDOW:
-			if Input.is_action_just_pressed("jump") and double_jump:
-				velocity.y = -jump_force + 200
-				double_jump = false
-			else:
-				velocity.y += gravity * delta
+		if Input.is_action_just_released("jump"):
+			velocity.y = 0
+		if Input.is_action_just_pressed("jump") and double_jump:
+			velocity.y = -jump_force + 200
+			double_jump = false
+		elif jump_window_timer>JUMP_WINDOW :
+			velocity.y += gravity * delta
 	else:
 		jump_window_timer = 0
 		if Input.is_action_just_pressed("jump"):
-			velocity.y = -jump_force
+			velocity.y += -jump_force
 			double_jump = true
+		if Input.is_action_pressed("attack") and is_on_floor():
+			is_attacking = true
+			if velocity.x == 0:
+				anim_tree.get("parameters/playback").travel("attack_stand")
+				$PivotNode/SwordSlash.play("sword_slash")
+			else:
+				anim_tree.get("parameters/playback").travel("attack_walk")
+				$PivotNode/SwordSlash.play("sword_slash")
+		if not Input.is_action_pressed("attack"):
+			is_attacking = false
 	move_and_slide()
 	
 	
 func _process(delta):
-	if Input.is_action_pressed("attack") and is_attacking == false:
-		is_attacking = true
-		$PivotNode/AnimatedSprite2D.play("attack")
-	if Input.is_action_just_released("attack"):
-		is_attacking = false
 	if velocity.x != 0 :
 		if velocity.x < 0:
 			$PivotNode.scale.x = -1
 		else:
 			$PivotNode.scale.x = 1
-	if is_on_floor():
-		if velocity.x != 0 :
-			if $PivotNode/AnimatedSprite2D.animation != "walk":
-				$PivotNode/AnimatedSprite2D.play("walk")
-		elif velocity.x == 0 and velocity.y == 0 :
-			if $PivotNode/AnimatedSprite2D.animation != "idle":
-				$PivotNode/AnimatedSprite2D.play("idle")
-	elif not is_on_floor():
-		if $PivotNode/AnimatedSprite2D.animation != "mid_air":
-				$PivotNode/AnimatedSprite2D.play("mid_air")
+	if not is_attacking :
+		if is_on_floor():
+			if velocity.x != 0 :
+				anim_tree.get("parameters/playback").travel("walk")
+			elif velocity.x == 0 and velocity.y == 0 :
+				anim_tree.get("parameters/playback").travel("idle")
+	if not is_on_floor():
+		anim_tree.get("parameters/playback").travel("mid_air")
+
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Enemies"):
+		enemy_pos = area.global_position
+		hit = true
+		if enemy_pos < global_position:
+			velocity.x += 400
+			velocity.y += -150
+		else:
+			velocity.x += -400
+			velocity.y += -150
+	await get_tree().create_timer(0.1).timeout
+	hit = false
