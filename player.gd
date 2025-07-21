265b86c2d4ac3e961611be9b8ed8045539cbd16e
hrakahraka <hamdihraka@gmail.com>
2025-07-21 14:44:50 +0100

    added death logic and death animation(next step respawn button, enemy death, level-up functionality)

--- player.gd
+++ player.gd
@@ -22,6 +22,7 @@ const JUMP_WINDOW = 0.01
 const KNOCKBACK_DURATION = 1
 var xp_points = 0
 var xp_for_next_level = 200
+var dead = false
 
 
 func _ready():
@@ -29,73 +30,82 @@ func _ready():
 
 
 func _physics_process(delta):
-	var direction = 0
-	if Input.is_action_pressed("move_right"):
-		direction += 1
-	if Input.is_action_pressed("move_left"):
-		direction -=  1
-	if direction != 0:
-		velocity.x = direction * speed
-	elif (not hit) and is_on_floor():
-		velocity.x = 0
-	if not is_on_floor():
-		jump_window_timer += delta
-		if Input.is_action_just_released("jump"):
-			velocity.y = 0
-		if Input.is_action_just_pressed("jump") and double_jump:
-			velocity.y = -jump_force + 200
-			double_jump = false
-		elif jump_window_timer>JUMP_WINDOW :
-			velocity.y += gravity * delta
-	else:
-		jump_window_timer = 0
-		if Input.is_action_just_pressed("jump"):
-			velocity.y += -jump_force
-			double_jump = true
-		if Input.is_action_pressed("attack") and is_on_floor():
-			is_attacking = true
-			if velocity.x == 0:
-				anim_tree.get("parameters/playback").travel("attack_stand")
-				$PivotNode/SwordSlash.play("sword_slash")
-			else:
-				anim_tree.get("parameters/playback").travel("attack_walk")
-				$PivotNode/SwordSlash.play("sword_slash")
-		if not Input.is_action_pressed("attack"):
-			is_attacking = false
-	move_and_slide()
-	
-	
-func _process(delta):
-	if velocity.x != 0 :
-		if velocity.x < 0:
-			$PivotNode.scale.x = -1
+	if HP_points == 0 and is_on_floor():
+			dead = true
+	if not dead:
+		var direction = 0
+		if Input.is_action_pressed("move_right"):
+			direction += 1
+		if Input.is_action_pressed("move_left"):
+			direction -=  1
+		if direction != 0:
+			velocity.x = direction * speed
+		elif (not hit) and is_on_floor():
+			velocity.x = 0
+		if not is_on_floor():
+			jump_window_timer += delta
+			if Input.is_action_just_released("jump"):
+				velocity.y = 0
+			if Input.is_action_just_pressed("jump") and double_jump:
+				velocity.y = -jump_force + 200
+				double_jump = false
+			elif jump_window_timer>JUMP_WINDOW :
+				velocity.y += gravity * delta
 		else:
-			$PivotNode.scale.x = 1
-	if not is_attacking :
-		if is_on_floor():
-			if velocity.x != 0 :
-				anim_tree.get("parameters/playback").travel("walk")
-			elif velocity.x == 0 and velocity.y == 0 :
-				anim_tree.get("parameters/playback").travel("idle")
-	if not is_on_floor():
-		anim_tree.get("parameters/playback").travel("mid_air")
+			jump_window_timer = 0
+			if Input.is_action_just_pressed("jump"):
+				velocity.y += -jump_force
+				double_jump = true
+			if Input.is_action_pressed("attack") and is_on_floor():
+				is_attacking = true
+				if velocity.x == 0:
+					anim_tree.get("parameters/playback").travel("attack_stand")
+					$PivotNode/SwordSlash.play("sword_slash")
+				else:
+					anim_tree.get("parameters/playback").travel("attack_walk")
+					$PivotNode/SwordSlash.play("sword_slash")
+			if not Input.is_action_pressed("attack"):
+				is_attacking = false
+		move_and_slide()
+
+
+func _process(delta):
+	if not dead:
+		if velocity.x != 0 :
+			if velocity.x < 0:
+				$PivotNode.scale.x = -1
+			else:
+				$PivotNode.scale.x = 1
+		if not is_attacking :
+			if is_on_floor():
+				if velocity.x != 0 :
+					anim_tree.get("parameters/playback").travel("walk")
+				elif velocity.x == 0 and velocity.y == 0 :
+					anim_tree.get("parameters/playback").travel("idle")
+		if not is_on_floor():
+			anim_tree.get("parameters/playback").travel("mid_air")
+	else :
+		anim_tree.get("parameters/playback").travel("death")
 
 
 func _on_hurtbox_area_entered(area: Area2D) -> void:
-	enemy = area.get_parent().get_parent()
-	if enemy.is_in_group("Enemies"):
-		enemy_pos = enemy.global_position
-		hit = true
-		HP_points = clamp(HP_points - enemy.damage_value, 0, max_health)
-		$HUD.update_heart()
-		if enemy_pos < global_position:
-			velocity.x += enemy.knockback_power
-			velocity.y += -150
-		else:
-			velocity.x += -enemy.knockback_power
-			velocity.y += -150
-	await get_tree().create_timer(0.1).timeout
-	hit = false
+	if not dead:
+		enemy = area.get_parent().get_parent()
+		if enemy.is_in_group("Enemies"):
+			enemy_pos = enemy.global_position
+			hit = true
+			HP_points = clamp(HP_points - enemy.damage_value, 0, max_health)
+			$HUD.update_heart()
+			if enemy_pos < global_position:
+				velocity.x += enemy.knockback_power
+				velocity.y += -150
+			else:
+				velocity.x += -enemy.knockback_power
+				velocity.y += -150
+		await get_tree().create_timer(0.1).timeout
+		hit = false
+
+
 func level_up():
 	max_health += 1
 	HP_points = max_health
